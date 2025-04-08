2a207d9b1f0aa593608ca8364a9f1c1bb77fef27
Nina Rosliakova <nrosliakova@kse.org.ua>
2025-04-08 22:37:10 +0300

    add: class graph func to print matrix and list func to convert in matrix and list

--- DM-Floyd-Warshall/DM-Floyd-Warshall/Program.cs
+++ DM-Floyd-Warshall/DM-Floyd-Warshall/Program.cs
@@ -1,8 +1,21 @@
 ﻿
+namespace DM_Floyd_Warshall;
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("qwerty");
+        //const int vertexAmount = 5;
+        //int[,] matrix = new int[5, 5] {
+        //    {0, 3, 8, 2, 1},
+        //    {3, 0, 5, int.MaxValue, 7},
+        //    {8, 5, 0, 1, 6},
+        //    {2, 4, 1, 0, 9},
+        //    {1, 7, 6, 9, 0}
+        //};
+        //Graph.PrintMatrix(matrix, vertexAmount);
+        //var aList = Graph.ConvertMatrixToList(matrix, vertexAmount);
+        //Graph.PrintList(aList);
+        //var m2 = Graph.ConvertListToMatrix(aList);
+        //Graph.PrintMatrix(m2, vertexAmount);
     }
 }
